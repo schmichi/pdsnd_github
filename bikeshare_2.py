b7bb14ea9fc14abdc68202b6ff8ced9b13659251
Michael Schmidt <schmichi88@gmail.com>
2023-06-30 12:05:10 +0200

    refactor: add new lines of code for checking if inout is valid

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -16,6 +16,9 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+    # check if unser input is valid 
+    # code here...
+
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
@@ -135,3 +138,6 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+
+
